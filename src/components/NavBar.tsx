6fb33d8fd430f647b9abc5fef67d99792e4bacbb
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2024-11-28 00:44:14 +0600

    worked a bit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="h-12 text-green-900 p-4 flex items-center justify-between border-b-2 border-b-green-900 uppercase">
+      <div className="text-xl">
+        <Link href="/">Khuda Lagse</Link>
+      </div>
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Notification = () => {
-  return <div>Notification</div>;
+  return (
+    <div className="h-12 bg-green-900 text-white px-4 flex items-center justify-center text-sm md:text-base cursor-pointer">
+      Free Delivery for all orders over 50 usd. Order your food NOW!
+    </div>
+  );
 };
 
 export default Notification;
