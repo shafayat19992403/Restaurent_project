2282bddc51b8788229815b5102c573acbca2dc61
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2024-11-27 22:39:53 +0600

    temporary structure is done

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const orderPage = () => {
+  return <div>orders</div>;
+};
+
+export default orderPage;
---
+++ src/app/product/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const singleProduct = () => {
+  return <div>SingleProductPage</div>;
+};
+
+export default singleProduct;
